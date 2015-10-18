2beafb38108af77d08e988c3ebf1d69ff098201d
linear-pulse <lpnet.linear.pulse@gmail.com>
2015-10-18 19:55:41 +0900

    Calculatorクラスを以下のように修正 ・(divide) 例外発生時、呼び出し元で期待する例外を返すように修正

--- src/main/java/org/rebk/sample/Calculator.java
+++ src/main/java/org/rebk/sample/Calculator.java
@@ -1,21 +1,53 @@
 package org.rebk.sample;
 
+/**
+ * Execute Calculation
+ */
 public class Calculator {
 
+    /**
+     * Execute Add
+     * @param a summand
+     * @param b addend
+     * @return Calculation result.
+     * */
     public int add(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Execute subtract
+     * @param a minuend
+     * @param b subtrahend
+     * @return Calculation result.
+     * */
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Execute multiply
+     * @param a multiplicand
+     * @param b multiplier
+     * @return Calculation result.
+     * */
     public int multiply(int a, int b) {
-        throw new UnsupportedOperationException();
+        return a * b;
     }
 
-    public int divide(int a, int b) {
-        throw new UnsupportedOperationException();
+    /**
+     * Execute divide
+     * @param a dividend
+     * @param b divisor
+     * @return Calculation result. The floor is computed and the remainder is thrown away.
+     * */
+    public int divide(int a, int b) throws IllegalArgumentException {
+        int result;
+        try{
+            result = a / b;
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return result;
     }
-
 }
